8e5330bee56e0592ceea5fb969065dee85755d7b
yuxuan3d <yuxuan3d@users.noreply.github.com>
2026-02-08 12:48:41 +0800

    Initial commit

---
+++ 3dpotion/src/potion/constants.js
@@ -0,0 +1,12 @@
+export const MODEL_OUTER = '/models/PotionBase_outer.glb'
+export const MODEL_INNER = '/models/PotionBase_inner.glb'
+export const MODEL_FRESNEL = '/models/PotionBase_fresnel.glb'
+export const MODEL_CAP = '/models/PotionBase_Cap.glb'
+
+export const POTION_RENDER_ORDER = {
+  innerGlass: 1,
+  outerGlass: 2,
+  innerRim: 2.5,
+  outerFresnel: 3,
+  cap: 4,
+}
